71810ff13939d5eefbac9246364ab69df48f544e
SoFuw <ejgk13@gmail.com>
2022-11-07 20:59:30 +0900

    move from win to linux

--- f_comparator_tb.v
+++ f_comparator_tb.v
@@ -1,4 +1,34 @@
+`timescale 1ns/1ps
+`include "./f_comparator.v"
 module f_comparator_tb;
 
+reg [31:0] in0;
+reg [31:0] in1;
+
+wire res;
+integer f,cnt;
+
+f_comparator u_f_comparator(
+    .in0(in0),
+    .in1(in1),
+    .res(res)
+);
+
+
+initial begin
+    f = $fopen("f_comparator_log.txt","w");
+    for(cnt = 0; cnt < 4096; cnt = cnt+ 1)
+    begin
+        in0 <= $random;
+        in1 <= $random;
+        $fwrite(f,"cnt : %d\n",cnt);
+        $fwrite(f,"in0 : %b\n",in0);
+        $fwrite(f,"in1 : %b\n",in1);
+        $fwrite(f,"out : %b\n\n",res);
+        #10;
+    end
+    $fclose(f);
+    $finish;
+end
 
 endmodule
